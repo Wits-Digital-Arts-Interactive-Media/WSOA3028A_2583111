8106bf8b1e23cd31bffa696c772442df782e0cc2
2583111 <soulsociety2004@gmail.com>
2024-04-27 22:48:43 +0200

    Java Test

---
+++ Scripts/menu.js
@@ -0,0 +1,25 @@
+const root =  "/WSOA3028A_2024"
+
+const menuItem = [
+    { name: "Home", href: root + "/index.html" },
+    { name: "Blog", href: `${root}/blogs/index.html` },
+    { name: "Essays", href: `${root}/essays/index.html` },
+    { name: "Portfolio", href: `${root}/portfolio/index.html` },
+    { name: "Design", href: `${root}/design/index.html` },
+]
+
+export function initialise(currentPage) {
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li")
+        if (currentPage != menuItem.name) {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)
+    }
+    nav.appendChild(ul)
+}
