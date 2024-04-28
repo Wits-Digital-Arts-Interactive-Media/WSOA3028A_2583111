536a24c033a1e608b133e0f6fa1a7b1ea311fdf1
2583111 <soulsociety2004@gmail.com>
2024-04-28 23:02:49 +0200

    fix? probably no

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -2,30 +2,32 @@ const root = "/WSOA3028A_2583111";
 
 const menuItems = [
     { name: "Home", href: root + "/index.html" },
-    { name: "Blogs", href: `${root}Blogs/index.html` },
+    { name: "Blogs", href: `${root}/Blogs/index.html` },
     { name: "Essay", href: `${root}/Essay/index.html` },
     { name: "Portfolios", href: `${root}/Portfolios/index.html` },
     { name: "Design", href: `${root}/Design/index.html` },
 ];
 
 export function initialise(currentPage) {
-    const nav = document.querySelector("header > nav");
-    const ul = document.createElement("ul");
+    document.addEventListener("DOMContentLoaded", function() {
+        const nav = document.querySelector("header > nav");
+        const ul = document.createElement("ul");
 
-    for (let menuItem of menuItems) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
+        for (let menuItem of menuItems) {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
 
-        if (currentPage !== menuItem.name) {
-            a.innerText = menuItem.name;
-            a.setAttribute("href", menuItem.href);
-            li.appendChild(a);
-        } else {
-            li.innerText = menuItem.name;
+            if (currentPage !== menuItem.name) {
+                a.innerText = menuItem.name;
+                a.setAttribute("href", menuItem.href);
+                li.appendChild(a);
+            } else {
+                li.innerText = menuItem.name;
+            }
+
+            ul.appendChild(li);
         }
 
-        ul.appendChild(li);
-    }
-
-    nav.appendChild(ul);
+        nav.appendChild(ul);
+    });
 }
