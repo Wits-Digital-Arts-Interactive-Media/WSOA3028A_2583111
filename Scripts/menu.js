1649fafab3ce89ddafa1648ebfb990486d928760
2583111 <soulsociety2004@gmail.com>
2024-04-28 20:23:30 +0200

    something something

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -8,19 +8,24 @@ const menuItems = [
     { name: "Design", href: `${root}/Design/index.html` },
 ];
 
-export function initialiseMenu (currentPage) {
-    const nav = document.querySelector ("header > nav")
-    const ul = document.createElement ("ul")
-    
+export function initialise(currentPage) {
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
+
     for (let menuItem of menuItems) {
-        const li = document.createElement("li")
-            if(currentPage !=menuItem.name){
-                const a = document.createElement("a")
-                a.innerText = menuItem.name
-                a.setAttribute("href", menuItem.href)
-                li.appendChild(a)
-            } else { li.innerText=menuItem.name }
-            ul.appendChild(li)
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        if (currentPage !== menuItem.name) {
+            a.innerText = menuItem.name;
+            a.setAttribute("href", menuItem.href);
+            li.appendChild(a);
+        } else {
+            li.innerText = menuItem.name;
         }
-        nav.appendChild(ul)
+
+        ul.appendChild(li);
     }
+
+    nav.appendChild(ul);
+}
