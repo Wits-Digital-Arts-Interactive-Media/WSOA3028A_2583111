42b13e6306e51b82ccd9b8c08f4b52a44b95294b
2583111 <soulsociety2004@gmail.com>
2024-04-28 23:11:22 +0200

    :( man....

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -2,32 +2,24 @@ const root = "/WSOA3028A_2583111";
 
 const menuItems = [
     { name: "Home", href: root + "/index.html" },
-    { name: "Blogs", href: `${root}/Blogs/index.html` },
+    { name: "Blogs", href: `${root}Blogs/index.html` },
     { name: "Essay", href: `${root}/Essay/index.html` },
     { name: "Portfolios", href: `${root}/Portfolios/index.html` },
     { name: "Design", href: `${root}/Design/index.html` },
-];
+]
 
 export function initialise(currentPage) {
-    document.addEventListener("DOMContentLoaded", function() {
-        const nav = document.querySelector("header > nav");
-        const ul = document.createElement("ul");
-
-        for (let menuItem of menuItems) {
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-
-            if (currentPage !== menuItem.name) {
-                a.innerText = menuItem.name;
-                a.setAttribute("href", menuItem.href);
-                li.appendChild(a);
-            } else {
-                li.innerText = menuItem.name;
-            }
-
-            ul.appendChild(li);
-        }
-
-        nav.appendChild(ul);
-    });
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li")
+        if (currentPage != menuItem.name) {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)
+    }
+    nav.appendChild(ul)
 }
