2a207f561a523d77af7d20e43d26208be9805a85
2583111 <soulsociety2004@gmail.com>
2024-04-28 17:36:58 +0200

    A13????????????????????????

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -2,25 +2,30 @@ const root = "/WSOA3028A_2583111";
 
 const menuItems = [
     { name: "Home", href: root + "/index.html" },
-    { name: "Blogs", href: `${root}/Blogs/index.html` },
-    { name: "Essay", href: `${root}/Essay/index.html` },
+    { name: "Blogs", href: "/Blogs/index.html" },
+    { name: "Essay", href: `/Essay/index.html` },
     { name: "Portfolios", href: `${root}/Portfolios/index.html` },
     { name: "Design", href: `${root}/Design/index.html` },
 ];
 
 export function initialise(currentPage) {
-    const nav = document.querySelector("header < nav");
+    const nav = document.querySelector("header > nav");
     const ul = document.createElement("ul");
+
     for (let menuItem of menuItems) {
         const li = document.createElement("li");
         const a = document.createElement("a");
-        a.innerText = menuItem.name;
-        a.setAttribute("href", menuItem.href);
-        if (currentPage === menuItem.name) {
-            a.classList.add("active");
+
+        if (currentPage !== menuItem.name) {
+            a.innerText = menuItem.name;
+            a.setAttribute("href", menuItem.href);
+            li.appendChild(a);
+        } else {
+            li.innerText = menuItem.name;
         }
-        li.appendChild(a);
+
         ul.appendChild(li);
     }
+
     nav.appendChild(ul);
 }
