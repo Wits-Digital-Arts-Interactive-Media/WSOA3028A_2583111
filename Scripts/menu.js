a1dc668b2c2b419d2621d53f6bf51b90ffe65217
2583111 <soulsociety2004@gmail.com>
2024-04-28 15:25:40 +0200

    A3 fix??????

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -8,28 +8,19 @@ const menuItems = [
     {name: "Design", href: `${root}/Design/index.html`},
 
 ];
-export function initialise(currentPage){
 
-    console.log("worked")
+export function initialise(currentPage) {
     const nav = document.querySelector("header > nav")
     const ul = document.createElement("ul")
-    for(let menuItem of menuItems)
-    {
+    for (let menuItem of menuItems) {
         const li = document.createElement("li")
-        if(currentPage != menuItem.name)
-        {
+        if (currentPage != menuItem.name) {
             const a = document.createElement("a")
             a.innerText = menuItem.name
             a.setAttribute("href", menuItem.href)
             li.appendChild(a)
-
-        }
-        else{
-            li.innerText = menuItem.name
-        }
+        } else { li.innerText = menuItem.name }
         ul.appendChild(li)
     }
     nav.appendChild(ul)
-
-    
 }
