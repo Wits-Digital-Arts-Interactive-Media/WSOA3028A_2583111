51d28fd92291b90fdc6c909c178963235f9ed424
2583111 <soulsociety2004@gmail.com>
2024-04-28 19:43:48 +0200

    menu test 10????????????

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -2,30 +2,24 @@ const root = "/WSOA3028A_2583111";
 
 const menuItems = [
     { name: "Home", href: root + "/index.html" },
-    { name: "Blogs", href: `${root}/Blogs/index.html` },
+    { name: "Blogs", href: `${root}Blogs/index.html` },
     { name: "Essay", href: `${root}/Essay/index.html` },
     { name: "Portfolios", href: `${root}/Portfolios/index.html` },
     { name: "Design", href: `${root}/Design/index.html` },
 ];
 
 export function initialise(currentPage) {
-    const nav = document.querySelector("header > nav");
-    const ul = document.createElement("ul");
-
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
     for (let menuItem of menuItems) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.innerText = menuItem.name;
-        a.setAttribute("href", menuItem.href);
-        li.appendChild(a);
-        ul.appendChild(li);
+        const li = document.createElement("li")
+        if (currentPage != menuItem.name) {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)
     }
-
-    nav.appendChild(ul);
-
-    // Mark the current page as active
-    const currentLink = ul.querySelector(`a[href*="${currentPage.toLowerCase()}"]`);
-    if (currentLink) {
-        currentLink.classList.add("active");
-    }
+    nav.appendChild(ul)
 }
