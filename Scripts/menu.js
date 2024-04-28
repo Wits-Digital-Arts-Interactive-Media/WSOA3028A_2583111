cbb9ce1c0c8d2ae4d2d7d587e1dfb982685e421b
2583111 <soulsociety2004@gmail.com>
2024-04-28 18:27:06 +0200

    Style test 3?

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -9,23 +9,17 @@ const menuItems = [
 ];
 
 export function initialise(currentPage) {
-    const nav = document.querySelector("header > nav");
-    const ul = document.createElement("ul");
-
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
     for (let menuItem of menuItems) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-
-        if (currentPage !== menuItem.name) {
-            a.innerText = menuItem.name;
-            a.setAttribute("href", menuItem.href);
-            li.appendChild(a);
-        } else {
-            li.innerText = menuItem.name;
-        }
-
-        ul.appendChild(li);
+        const li = document.createElement("li")
+        if (currentPage != menuItem.name) {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)
     }
-
-    nav.appendChild(ul);
+    nav.appendChild(ul)
 }
