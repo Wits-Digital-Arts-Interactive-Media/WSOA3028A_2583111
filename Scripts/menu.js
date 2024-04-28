19df827e612a931373be1c1a561f819f34eea7cd
2583111 <soulsociety2004@gmail.com>
2024-04-28 16:30:41 +0200

    A8 test ? fix?

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,26 +1,26 @@
-const root = "/WSOA3028A_2583111";
+const root =  "/WSOA3028A_2583111"
 
 const menuItems = [
-    { name: "Home", href: root + "/index.html" },
-    { name: "Blogs", href: `${root}/blogs/index.html` },
-    { name: "Essays", href: `${root}/essays/index.html` },
-    { name: "Portfolio", href: `${root}/portfolio/index.html` },
-    { name: "Design", href: `${root}/design/index.html` },
-];
+    {name: "Home", href: root + "/index.html"},
+    {name: "Blog Posts", href: `${root}/Blogs/index.html`},
+    {name: "Essay", href: `${root}/Essays/index.html`},
+    {name: "Portfolio", href: `${root}/Portfolio/index.html`},
+    {name: "Design", href: `${root}/Design/index.html`},
+
+]
 
 export function initialise(currentPage) {
-    const nav = document.querySelector("header > nav");
-    const ul = document.createElement("ul");
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
     for (let menuItem of menuItems) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.innerText = menuItem.name;
-        a.setAttribute("href", menuItem.href);
-        if (currentPage === menuItem.name) {
-            a.classList.add("active"); // Add a class to the active menu item
-        }
-        li.appendChild(a);
-        ul.appendChild(li);
+        const li = document.createElement("li")
+        if (currentPage != menuItem.name) {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)
     }
-    nav.appendChild(ul);
+    nav.appendChild(ul)
 }
