24502190272df1a5046654a02f72a46ac737478f
2583111 <soulsociety2004@gmail.com>
2024-05-27 09:55:07 +0200

    cxccv

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -7,22 +7,21 @@ const menuItems = [
     { name: "Portfolio", href: `${root}/Portfolios/index.html` },
     { name: "Design", href: `${root}/Design/index.html` },
     { name: "Profile", href: `${root}/Profile/index.html` },
-
-]
+];
 
 export function initialise(currentPage) {
-    const nav = document.querySelector("header > nav")
-    const ul = document.createElement("ul")
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
     for (let menuItem of menuItems) {
-        const li = document.createElement("li")
-        const a = document.createElement("a")
-        a.innerText = menuItem.name
-        a.setAttribute("href", menuItem.href)
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.innerText = menuItem.name;
+        a.setAttribute("href", menuItem.href);
         if (currentPage === menuItem.name) {
-            a.classList.add("current"); 
+            a.classList.add("current");
         }
-        li.appendChild(a)
-        ul.appendChild(li)
+        li.appendChild(a);
+        ul.appendChild(li);
     }
-    nav.appendChild(ul)
+    nav.appendChild(ul);
 }
