5b79a0a0cd981aa069a0a1e6d315f8b4924443a7
2583111 <soulsociety2004@gmail.com>
2024-06-23 09:00:57 +0200

    Testo

--- Scripts/interactiveImg.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const artSlides = document.querySelectorAll('.art-slide figure img');
-    artSlides.forEach(img => {
-        img.addEventListener('click', () => {
-            window.location.href = `${window.location.origin}/Portfolios/index.html`;
-        });
-    });
-});
