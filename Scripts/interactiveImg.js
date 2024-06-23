7c8b30d467390164f75f494f320621d9b9fecb54
2583111 <soulsociety2004@gmail.com>
2024-06-23 08:47:43 +0200

    New Test Java

---
+++ Scripts/interactiveImg.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const artSlides = document.querySelectorAll('.art-slide figure img');
+
+    artSlides.forEach(img => {
+        img.addEventListener('click', () => {
+            window.location.href = `${window.location.origin}/Portfolios/index.html`;
+        });
+    });
+});
