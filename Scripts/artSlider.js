986332e7d4586af981b7c4c95b3e9dae3a26b182
2583111 <soulsociety2004@gmail.com>
2024-06-23 07:52:17 +0200

    removing some js

--- Scripts/artSlider.js
+++ Scripts/artSlider.js
@@ -41,38 +41,3 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(nextArtSlide, 5000);
     setInterval(nextWritingSlide, 6000);
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-    let artIndex = 0;
-    const artSlides = document.querySelectorAll(".art-slide");
-
-    function showArtSlide(index) {
-        artSlides.forEach((slide, i) => {
-            slide.classList.toggle("active", i === index);
-        });
-    }
-
-    function nextArtSlide() {
-        artIndex = (artIndex + 1) % artSlides.length;
-        showArtSlide(artIndex);
-    }
-
-    function prevArtSlide() {
-        artIndex = (artIndex - 1 + artSlides.length) % artSlides.length;
-        showArtSlide(artIndex);
-    }
-
-    document.querySelector(".prev-slide").addEventListener("click", prevArtSlide);
-    document.querySelector(".next-slide").addEventListener("click", nextArtSlide);
-
-    // Adding click event listeners to art slides
-    artSlides.forEach((slide) => {
-        slide.addEventListener("click", () => {
-            window.location.href = "../Portfolios/index.html"; 
-        });
-    });
-
-    showArtSlide(artIndex);
-
-    setInterval(nextArtSlide, 5000); // Auto slide every 5 seconds
-});
